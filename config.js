929502d3b5ebfd6be1d5cfe2965367dd5d917519
sreesha2015 <sreeni87@gmail.com>
2025-08-18 17:57:25 +0530

    Delete config.js

--- config.js
+++
@@ -1,3 +0,0 @@
-// Copy this file, fill in your project details, and keep it private if possible.
-window.SUPABASE_URL = "https://hrqmgieuindcwipgcvdj.supabase.co";
-window.SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhycW1naWV1aW5kY3dpcGdjdmRqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTUxOTM1NDEsImV4cCI6MjA3MDc2OTU0MX0.idkUvys-Fc25uA9Nre2E0V5X5qHxPcnlVyLL4OE0E9Q";
